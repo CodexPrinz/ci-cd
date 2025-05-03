99d2a9c7524c3f92a344f6b7ee7c566a13ba66c1
Prinz Preince <prinzpreince@live.com>
2025-05-03 11:13:20 +0200

    test getAll

--- src/main/java/com/projects/ci_cd/model/Product.java
+++ src/main/java/com/projects/ci_cd/model/Product.java
@@ -40,4 +40,13 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/test/java/com/projects/ci_cd/CiCdApplicationTests.java
+++ src/test/java/com/projects/ci_cd/CiCdApplicationTests.java
@@ -1,13 +1,40 @@
 package com.projects.ci_cd;
 
+import com.projects.ci_cd.controller.ProductController;
+import com.projects.ci_cd.model.Product;
+import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiCdApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(CiCdApplicationTests.class);
+	@Autowired
+	private ProductController productController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testProductController() {
+//		 Test the ProductController methods here
+//		 You can use MockMvc to test the endpoints
+//		 Example: mockMvc.perform(get("/api/products")).andExpect(status().isOk());
+	}
+
+	@Test
+	void testGetAllProducts() {
+		ResponseEntity<Iterable<Product>> response = productController.getAllProducts();
+		log.info("Response: {}, {}", response.getBody(), response.getStatusCode());
+		assertEquals(200, response.getStatusCodeValue());
 	}
 
 }
